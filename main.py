d4dbd881305c1948314604b962ae723f5f98660a
Ping <umitcan.yc@gmail.com>
2020-07-16 19:39:36 +0300

    update

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pyautogui as sc
-import time
 
 
 class Bot:
@@ -7,17 +6,18 @@ class Bot:
         (self.width, self.height) = sc.size()
         self.query = query
         self.speed = speed
+        self.old = 1
 
     def start(self):
-        img = sc.screenshot(region=(self.width / 2 - self.width / 8 + 10, self.height / 2 - self.height / 4, 100, 100))
+        img = sc.screenshot(region=(209, 371, 100, 100))
         colors = img.getcolors()
         if colors:
             for RGB in colors:
-                if RGB[1] == (83, 83, 83) or RGB[1] == (169, 169, 169) or RGB[1] == (115, 115, 115):
+                if RGB[1] == (83, 83, 83):
                     if RGB[0] > 100:
                         sc.press("up")
+                        print("Hit")
                         # img.save("detected/" + self.query.__str__() + ".png")
-                        # print(self.query, RGB[0])
         self.query += 1
 
     def getSpeed(self):
